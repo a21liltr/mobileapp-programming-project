91041c36b56300a674577f19a2f0dff20ca42d22
a21liltr <a21liltr@student.his.se>
2023-05-02 15:25:31 +0200

    Added url to json data.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String json = "https://mobprog.webug.se/json-api?login=a21liltr";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,4 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+
+    /*
+        TODO: RecyclerView med JSON-data
+          JSON-object w/ ID, Login, 3 <= attributes
+          5 <= JSON-object
+
+        TODO: Separate "about"-screen
+          -Target audience for app
+
+        TODO: Include screenshots/videos
+      */
 }
