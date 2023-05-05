da85f82f624589cbd877f7aada6ae3f92ec673cc
a21liltr <a21liltr@student.his.se>
2023-05-05 14:29:10 +0200

    Added picture to drawable.

--- app/src/main/java/com/example/project/About.java
+++ app/src/main/java/com/example/project/About.java
@@ -1,14 +1,23 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class About extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ImageView header;
+    String url =
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        toolbar = findViewById(R.id.toolbar_collapsing);
+        setSupportActionBar(toolbar);
     }
 }
